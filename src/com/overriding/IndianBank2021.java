ade3b84a5d0db155eb554e24ce07ec1675956598
Sabasiva06 <sabasiva06@gmail.com>
2023-01-12 10:48:59 +0530

    B works

--- src/com/overriding/IndianBank2021.java
+++ src/com/overriding/IndianBank2021.java
@@ -1,6 +1,8 @@
 package com.overriding;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -31,6 +33,18 @@ public class IndianBank2021 {
 		Set<Entry<Character, Integer>> entrySet = map.entrySet();
 		for (Entry<Character, Integer> entry : entrySet) {
 			System.out.println(entry);
+		}
+
+		List<String> li = new ArrayList<String>();
+		li.add("a");
+		li.add("b");
+		li.add("c");
+		li.add("d");
+		li.add("f");
+
+		for (int i = 0; i < li.size(); i++) {
+			String string = li.get(i);
+			System.out.println(string);
 		}
 
 	}
